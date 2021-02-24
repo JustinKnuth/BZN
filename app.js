4c362fcbc745d3d15ccf746b6251c00574369a48
Justin Kay <justinkay@Justins-MacBook-Pro.local>
2021-02-24 16:33:19 -0500

    Rendered data on the page

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // const { default: axios } = require("axios")
 
 // Main variables and endpoints here.
-const DOMAIN = 'http://cors-anywhere.herokuapp.com/http://newsapi.org/'
-const apiKey = '90227dcd441a4362b5c3669eb2b7e229'
-const url = `${DOMAIN}v2/top-headlines?apiKey=${apiKey}`
-const leftPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=cnn.com,nbcnews.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
-const midPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=apnews.com,pbs.org&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
-const rightPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=nypost.com,foxnews.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
+
+const leftPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=cnn.com,nbcnews.com&sortBy=publishedAt&language=en&apiKey=000cb07e70e54afda050fe51fd532b10'
+const midPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=apnews.com,pbs.org&sortBy=publishedAt&language=en&apiKey=000cb07e70e54afda050fe51fd532b10'
+const rightPotusURL = 'https://newsapi.org/v2/everything?qInTitle=Biden&domains=nypost.com,foxnews.com&sortBy=publishedAt&language=en&apiKey=000cb07e70e54afda050fe51fd532b10'
 const leftHouseURL = 'https://newsapi.org/v2/everything?qInTitle=congress&domains=cnn.com,nbcnews.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
 const midHouseURL = 'https://newsapi.org/v2/everything?qInTitle=congress&domains=apnews.com,pbs.org&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
 const rightHouseURL = 'https://newsapi.org/v2/everything?qInTitle=congress&domains=nypost.com,foxnews.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
@@ -16,16 +14,83 @@ const rightSenateURL = 'https://newsapi.org/v2/everything?qInTitle=senate&domain
 const leftScotusURL = 'https://newsapi.org/v2/everything?qInTitle=supreme-court&domains=cnn.com,nbcnews.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
 const midScotusURL = 'https://newsapi.org/v2/everything?qInTitle=supreme-court&domains=apnews.com,pbs.org&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
 const rightScotusURL = 'https://newsapi.org/v2/everything?qInTitle=supreme-court&domains=foxnews.com,nypost.com&sortBy=publishedAt&language=en&apiKey=90227dcd441a4362b5c3669eb2b7e229'
-console.log(url)
+
 
 // AXIOS API CALL 
 
 const fetchNews = async () => {
   const cors = 'http://cors-anywhere.herokuapp.com/'
   try {
+    // let leftPotus = await axios.get(`${cors}${leftPotusURL}`);
+    let midPotus = await axios.get(`${cors}${midPotusURL}`);
+    let rightPotus = await axios.get(`${cors}${rightPotusURL}`);
+    // let leftHouse = await axios.get(`${cors}${leftHouseURL}`);
+    // let midHouse = await axios.get(`${cors}${midHouseURL}`);
+    // let rightHouse = await axios.get(`${cors}${rightHouseURL}`);
+    // let leftSenate = await axios.get(`${cors}${leftSenateURL}`);
+    // let midSenate = await axios.get(`${cors}${midSenateURL}`);
+    // let rightSenate = await axios.get(`${cors}${rightSenateURL}`);
+    // let leftScotus = await axios.get(`${cors}${leftScotusURL}`);
+    // let midScotus = await axios.get(`${cors}${midScotusURL}`);
+    // let rightScotus = await axios.get(`${cors}${rightScotusURL}`);
     
-    let response = await axios.get(`${cors}${leftPotusURL}`);
-    console.log(response)
+    // leftPotus.data.articles.forEach((article) => {
+    //   const leftPotusSec = document.querySelector("#left-potus")
+      
+    //   const rightPotusSec = document.querySelector('#right-potus')
+
+    //   const h2 = document.createElement('h2')
+    //   const p = document.createElement('p')
+    //   const img = document.createElement('img')
+    //   img.src = article.urlToImage
+    //   let network = article.source.name
+    //   let title = article.title
+    //   h2.textContent = network
+    //   p.textContent = title
+    //   leftPotusSec.append(h2)
+    //   leftPotusSec.append(p)
+    //   leftPotusSec.append(img)
+      
+
+    // })
+    midPotus.data.articles.forEach((article) => {
+      const midPotusSec = document.querySelector('#mid-potus')
+
+      const h2 = document.createElement('h2')
+      const p = document.createElement('p')
+      const img = document.createElement('img')
+     img.src = article.urlToImage
+      let network = article.source.name
+      let title = article.title
+      h2.textContent = network
+      p.textContent = title
+      midPotusSec.append(h2)
+      midPotusSec.append(p)
+      midPotusSec.append(img)
+
+
+    })
+    
+    rightPotus.data.articles.forEach((article) => {
+      const midPotusSec = document.querySelector('#right-potus')
+
+      const h2 = document.createElement('h2')
+      const p = document.createElement('p')
+      const img = document.createElement('img')
+     img.src = article.urlToImage
+      let network = article.source.name
+      let title = article.title
+      h2.textContent = network
+      p.textContent = title
+      midPotusSec.append(h2)
+      midPotusSec.append(p)
+      midPotusSec.append(img)
+
+
+      })
+
+    
+    console.log(midPotus.data.articles)
     
   }
   catch (err) {
